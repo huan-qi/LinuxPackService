8ac587becb99069af506fcf05071533e284d2283
qihuan <916884286@qq.com>
2022-12-14 11:22:59 +0800

    commit2

--- LinuxPackFileConsole/Program.cs
+++ LinuxPackFileConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using LinuxPackFile;
+using System.Xml;
+using System.Linq;
 
 namespace LinuxPackFileConsole
 {
@@ -8,18 +10,18 @@ namespace LinuxPackFileConsole
         {
             if (args == null || args.Length < 5)
             {
-                Console.WriteLine("请输入参数 1. 券商标识（eg: ZheShang） 2. 券商名称（eg: 浙商金融终端）3. 版本号（eg: 2.3.0.1） 4. 是发是预发布版本（eg: true）5. 输出路径");
+                Console.WriteLine("请输入参数 1. 券商标识（eg: ZheShang）2. 版本号（eg: 2.3.0.1）3. 是发是预发布版本（eg: true）4. 输出路径");
                 return;
             }
 
             var productType = args[0];
-            var productName = args[1];
+            var productName = GetProductName(productType);
             var productVersion = args[2];
             var isPreRelease = args[3];
             var outputPath = args[4];
 
             var baseDirectory = AppDomain.CurrentDomain.BaseDirectory.ToString();
-            string packTemplateFolderPathTemp = Path.Combine(baseDirectory, "Temp");     //定制打包模板生产位置
+            string packTemplateFolderPathTemp = Path.Combine(baseDirectory, "Temp");
             string amdAppPath = Path.Combine(baseDirectory, "linux-x64");
             string armAppPath = Path.Combine(baseDirectory, "linux-arm64");
 
@@ -64,6 +66,14 @@ namespace LinuxPackFileConsole
 
             //修改build.sh dos2unix
             //chmod a+x build.sh
+        }
+
+        private static string GetProductName(string productType)
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            xmlDocument.Load("ProductConfig.xml");
+            var node = xmlDocument.SelectSingleNode($"Product/{productType}");
+            return node.InnerText;
         }
     }
 }
